78459e1a95b467a8082e06f60891be5c110527fc
meag_godoy <meag.godoy@gmail.com>
2026-02-22 09:05:15 -0300

    Add files via upload

---
+++ listas.py
@@ -0,0 +1,30 @@
+#Listas: coleção de elementos
+frutas = ["maçã", "pera", "laranja"]
+#a lista começa em índice 0
+
+#métodos:
+frutas.append("uva") #adiciona um elemento ao final da lista
+print(frutas)
+
+frutas.insert(1, "banana") #adiciona um elemento em um índice específico
+print(frutas)
+
+frutas.remove("uva") #remove a primeira ocorrência do elemento
+print(frutas)
+
+frutaRemovida = frutas.pop(2) #remove elemento pelo índice e retorna
+print(frutas)
+print(frutaRemovida)
+
+frutas.sort() #ordena a lista em ordem ascendente de elementos
+print(frutas)
+
+frutas.reverse() #inverte ordem dos elementos
+print(frutas)
+
+#Listas de compreensão:
+num = range(1, 11)
+#baseado na lista num, vamos criar outra
+#novaLista = [expressão for número in lista if condição]
+quadradosPares = [x ** 2 for x in num if x % 2 == 0] 
+print(quadradosPares) #irá apresentar apenas os quadrados pares na lista de números de 1 a 10
---
+++ mundo.py
@@ -0,0 +1,16 @@
+print("Olá, mundo!")
+
+frutas = ["maçã", "banana", "pera"]
+
+for fruta in frutas:
+    print(fruta)
+#não foi preciso criar a variável fruta, é como se fosse criado na hora mesmo
+
+for num in range(1,6):
+    print(num * 2)
+#o range funciona como um limite matemático: [1, 6[
+
+num = 0
+while num < 5:
+    print(num)
+    num += 1
