c2f197a7dc74771edf9aff6151d207e094fccb7a
meag_godoy <meag.godoy@gmail.com>
2026-03-02 14:09:22 -0300

    Update EX01.py

--- Python/Exercícios/Exercícios Pandas/EX01.py
+++ Python/Exercícios/Exercícios Pandas/EX01.py
@@ -5,7 +5,8 @@
 import pandas as pd
 
 #df = data frame | r = read, também pra não dar problema com o caminho do arquivo
-df = pd.read_excel(r"C:\Users\maria\meag.dev\Python\Exercícios Operadores\votacao_individual_municipio.xlsx")
+df = pd.read_excel(r"C:\Users\maria\meag.dev\Python\Exercícios\Exercícios Pandas\votacao_individual_municipio.xlsx")
 
 
 print(df['Voto'].value_counts(normalize=True).round(3)*100)
+
