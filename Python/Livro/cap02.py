62de2a5c8e512af729e024f23bfbf118aae91bad
meag_godoy <meag.godoy@gmail.com>
2026-05-01 16:11:32 -0300

    Add files via upload

---
+++ Python/Livro/cap02.py
@@ -0,0 +1,24 @@
+#Exercício 2.1:
+print(10 + 20 * 30)
+
+#exercício 2.2:
+print(10 % 3 * 10 ** 2 + 1 - 10 * 4 / 2)
+
+#Exercício 2.3:
+print("Olá, Duda!")
+
+#Exercício 2.4:
+a = 3
+b = 5
+print(2 * a * 3 * b)
+
+#Exercício 2.5:
+a = 2
+b = 3
+c = 5
+print(a + b + c)
+
+#Exercício 2.6:
+salario = 750
+aumento = 15
+print(salario + (salario * aumento / 100))
---
+++ Python/Livro/cap03.py
@@ -0,0 +1,25 @@
+#Exercício 3.4:
+salario = 1500
+imposto = salario > 1200
+print(imposto)
+
+#Exercício 3.5:
+A = 1
+B = 2
+C = True
+D = False
+expressao = A > B and C or D
+print(expressao)
+
+#Exercício 3.6:
+materia1 = 7
+materia2 = 6
+materia3 = 9
+aprovacao = (materia1 > 7) and (materia2 > 7) and (materia3 > 7)
+print(aprovacao)
+
+#Exercício 3.7:
+a = int(input("Digite um valor: "))
+b = int(input("Digite um valor: "))
+soma = a + b
+print("A soma é: ", soma)
